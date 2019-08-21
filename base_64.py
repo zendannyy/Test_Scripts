ec2e16f2ccf17ab4b3b8939e03b9e30aac7bf5ee
insert github real name here <insert github email here>
2019-08-21 15:32:27 -0700

    update

--- base_64.py
+++ base_64.py
@@ -12,3 +12,18 @@ def base64_decode(input,errors='strict') : # this procudes no results, errors, e
 	else:
 		print ("No output")
 
+'''
+from base64 import b64decode
+import binascii
+def base64(string, base):
+    string = b64decode(string)
+    string = ensure_str(string)
+    if string in decoded:
+        Quit()
+    print(g + ' Decoded from Base64 : %s' % (string))
+    decoded.append(string)
+    decode(string, 'none')
+    decode(base, 'b64')
+    Quit()
+'''
+# https://github.com/s0md3v/Decodify/blob/master/dcode
