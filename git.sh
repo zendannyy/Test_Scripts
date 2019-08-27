a6de47ef48ee7d71ffabeb1032aa78d1fc65f393
zendannyy <Dzendejas@sfo.yearup.org>
2019-08-26 21:49:58 -0700

    updating

--- git.sh
+++ git.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # from local dir
-chmod +x
-git init
-git fetch
-git add -A
-git commit -m "updating"
+# push(){
+	chmod +x
+	git init
+	git fetch
+	git add -A
+	git commit -m "updating"
 
-# git remote add origin repo URL
-# git remote -v
-git pull
-git push -u origin master
+	# git remote add origin repo URL
+	# git remote -v
+	git pull
+	git push -u origin master
