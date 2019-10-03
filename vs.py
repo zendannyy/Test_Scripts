40f5e6236d3797285ecaa9d387e549154b52bdfc
zendannyy <Dzendejas@sfo.yearup.org>
2019-10-03 15:52:00 -0700

    updating

--- vs.py
+++ vs.py
@@ -1,14 +1,15 @@
 #usr/bin/python
 # following script uses requests module to make GET requests w/ vt's API  seems to only work on python3 
 import requests			
-params = {'apikey': , 'resource': hashes_file}		# Changed order of params here 
+params = {'apikey': '', 'resource': 'hashes_file'}		# Changed order of params here 
 response = requests.get('https://www.virustotal.com/api/v2/file/report', params)
 r = response.json()
-for hash in hashes_file:
+print(r['positives'])	# using dictionaries to call keys 
+# for hash in hashes_file:
 	if r['response'] >= 10:	# greater than 10 hits , using dictionaries to call key:value 
-		print ("malicious threshold met, investigate")
+		print ("malicious threshold met, investigate") , print(r['md5'])
 	else:
-		print ("malicious confidence & threshold not met")
-		print (json.response())
+		print ("malicious confidence & threshold not met for") , print(r['md5'])
+		print (json.response())		# if full json output is prefererred 
 
 #  url = 'https://www.virustotal.com/vtapi'/v2/file/scan' 	#for file scan
