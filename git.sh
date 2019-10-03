be47e9217695c5f7ede94bbc947be87486f085f6
zendannyy <Dzendejas@sfo.yearup.org>
2019-10-03 15:45:28 -0700

    updating

--- git.sh
+++ git.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #from local dir
 # chmod +x
+#git init
+#git fetch
+#git add -A
+#git commit -m "updating"
+
+function current_branch() {
+	git_current_branch
+}
+
 git init
 git fetch
 git add -A
 git commit -m "updating"
 
-git pull
-git push -u origin master
-# push(){
+git pull $current_branch
+git push $current_branch	# -u origin master
 # git remote add origin repo URL
 # git remote -v
