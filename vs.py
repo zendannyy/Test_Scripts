9812cc26f8bef54c672accca2f636164d6c6cb8a
zen <zen@zens-MacBook-Pro.local>
2020-07-20 22:21:58 -0700

    Added input functionality for vs.py, will return hits and action to take

--- vs.py
+++ vs.py
@@ -1,17 +1,44 @@
-#usr/bin/python
-# following script uses requests module to make GET requests w/ vt's API  seems to only work on python3 
-import requests			
-import datetime
-params = {'apikey': '', 'resource': 'hashes_file'}		# Changed order of params here 
-response = requests.get('https://www.virustotal.com/api/v2/file/report', params)
-r = response.json()
-print(r['positives'])	# using dictionaries to call keys 
-# for hash in hashes_file:
-	if r['response'] >= 10:	# greater than 10 hits , using dictionaries to call key:value 
-		print ("malicious threshold met, investigate") , print(r['md5'])
-	else:
-		print ("malicious confidence & threshold not met for") , print(r['md5'])
-		formatted_text = r.json()
-		# print (json.response())		# if full json output is prefererred 
+#!/usr/bin/python3
+'''following script uses requests to make GET requests w/ vt's API'''
+import os
+import sys
+import requests
 
-#  url = 'https://www.virustotal.com/vtapi'/v2/file/scan' 	#for file scan
+if len(sys.argv) < 2:
+    print("Usage: Run script as follows: vs.py hash ")
+    sys.exit()
+
+hash = ' '.join(sys.argv[1:])
+
+class VTSafe:
+    '''docstring for VTSafe class'''
+    def __init__(self):
+        '''To retrieve the key'''
+        super(VTSafe, self).__init__()
+        self.apikey = os.environ.get("APIKEY")
+        self.params = {'apikey': self.apikey, 'resource': hash}
+
+    def get_request(self):
+        '''Get request for hash that is specified by user as input'''
+
+        # use 4107de199cda7dec5015bc92e0e48c35 when needing to test for a hash of <= 10
+        # use f3774e816bd8562fcb1e1ef3f12e5e70 when testting for a hash of >= 10
+        response = requests.get("https://www.virustotal.com/vtapi/v2/file/report", self.params)
+        r = response.json()
+        print(r['positives'])
+        # print(response.json())
+
+        if r['positives'] >= 10:	# greater than 10 hits, using dict to call value
+            print("malicious threshold met, investigate")
+            print(r['permalink'])
+        else:
+            print("malicious confidence & threshold not met for"), print(r['md5'])
+
+if __name__ == '__main__':
+    vt = VTSafe()
+    vt.get_request()
+
+# url = 'https://www.virustotal.com/vtapi'/v2/file/scan' 	#for file scan
+
+# Combine vs.py and vt_safe
+# Done
