6238efb7e44a6a2fdfc8d2fda84e8706c2a63727
zendannyy <dannyzen024@gmail.com>
2021-11-20 20:28:57 -0800

    updating files

--- git.sh
+++ git.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
-#from local dir
+#!/usr/bin/env bash
 # chmod +x
-#git init
-#git fetch
-#git add -A
-#git commit -m "updating"
+
+# How to use this script
+function display_usage() {
+  echo -e "Run script like this: \n./git.sh < within a git project dir>"
+}
+
+if [[ ( "${@}" == "--help") ||  "${@}" == "-h" ]]; then 
+	display_usage
+	exit 0
+fi
 
 function current_branch() {
 	git_current_branch
@@ -12,10 +17,9 @@ function current_branch() {
 
 git init
 git fetch
-git add -A
-git commit -m "updating"
+git add -A; git commit -m "updating files"
 
 git pull $current_branch
-git push $current_branch	# -u origin master
+git push $current_branch
 # git remote add origin repo URL
 # git remote -v
